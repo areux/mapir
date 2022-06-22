0728f422d14c9a8d44e35a710bcabd41542e8382
Mehdi Abedi <ilkedmode@gmail.com>
2022-06-22 17:17:38 +0430

    map routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Mapir from "mapir-react-component";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import polyline from 'google-polyline';
 
 function App() {
   return (
@@ -41,7 +42,57 @@ function App2() {
   const [center, setCenter] = useState([
     51.6660, // lng
     32.6539, // lat
-  ])
+  ]);
+
+  const [geojson, setGeojson] = useState({});
+  const [coords, setCoords] = useState({
+    firstLng: center[0],
+    firstLat: center[1],
+    // 32.663346, 51.701991 (ahmad abad)
+    // 32.680286, 51.652407 (bahonar)
+    secondLng: "51.652407",
+    secondLat: "32.680286",
+  })
+
+  useEffect(() => {
+    fetch(
+      "https://map.ir/routes/route/v1/driving/" +
+      coords.firstLng +
+      "," +
+      coords.firstLat +
+      ";" +
+      coords.secondLng +
+      "," +
+      coords.secondLat,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          'x-api-key': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjIwMjRiZDBlMDA3MjFlMzQwMDkwNmZmZDE4OTE1YWZjZTZkZjQwNmI4NDc2NmU2YjczY2RkZTRiN2UxNzRlMjgzNmU5YjQ5ZmZiYTYzZmEyIn0.eyJhdWQiOiIxODM0OCIsImp0aSI6IjIwMjRiZDBlMDA3MjFlMzQwMDkwNmZmZDE4OTE1YWZjZTZkZjQwNmI4NDc2NmU2YjczY2RkZTRiN2UxNzRlMjgzNmU5YjQ5ZmZiYTYzZmEyIiwiaWF0IjoxNjU1MDQ2MzAyLCJuYmYiOjE2NTUwNDYzMDIsImV4cCI6MTY1NzYzODMwMiwic3ViIjoiIiwic2NvcGVzIjpbImJhc2ljIl19.QcgDk2vIwE8ia3iwmv7ZZIc4--4_nGbJ-ViTCtrdJp8AEX9BaGXJp-MJvUrDUAAJRP5GQX-SiaD2m33bCtYQX3LgedZmxcB43gmJgoa5TH2TzfWgzWfvNHzWn9EJy4sAF0nZk9TL_MYk79bDCTssy_YU32oA-EPXYaUzzeGLZecuXb5jexzk1eD59pi6370yaoGWIucmN_P33sSEBSTH9yANM7FdhUFA5zDBUbkumsy9Xb5OlPzlL4i01YzK9E7o3IW6dF74AX7EVmYXN5WoNo5WgMnC8_G0BYSc8yT6Wf-yaB6PC_dMCglVZJLcCjeKKAxFm4rTPhxvPZ26c7AbOw', //Mapir access token
+        }
+      }
+    )
+      .then(res => res.json())
+      .then(result => {
+        var decodeResult = polyline.decode(result.routes[0].geometry);
+        var reverseLatLon = decodeResult.map(items => items.reverse());
+        var geojsonpoint = {
+          type: "FeatureCollection",
+          features: [
+            {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "LineString",
+                coordinates: reverseLatLon
+              }
+            }
+          ]
+        };
+        setGeojson(geojsonpoint);
+      });
+  }, []);
+
   return (
     <>
       <div className="absolute !h-screen !w-screen pointer-events-none z-[90]" >
@@ -84,7 +135,12 @@ function App2() {
           Map={Map}
           center={center}
           onDragEnd={(e) => { console.log(e) }}
-        />
+        >
+          {/* <Mapir.GeoJSONLayer
+            data={geojson}
+            linePaint={{ "line-color": "red", "line-width": 5 }}
+          /> */}
+        </Mapir>
       </div>
     </>
   )
